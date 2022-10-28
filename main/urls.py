41d0438dba3ce8966587d04773676cf704e819c2
carlosmca44 <carlosmca4424@gmail.com>
2022-10-28 16:07:21 -0400

    link from login to home

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.auth_login, name='login'),
+    path('home/', views.home, name='home')
 ]
--- main/views.py
+++ main/views.py
@@ -6,9 +6,13 @@ def auth_login(request):
     if request.method == 'POST':
         form = LoginAuth(request.POST)
         if form.is_valid():
-            redirect('home')
+            return redirect('home')
     else:
         form = LoginAuth()
 
     context = {'form': form}
     return render(request, 'login/login.html', context)
+
+
+def home(request):
+    return render(request, 'home/home.html')
